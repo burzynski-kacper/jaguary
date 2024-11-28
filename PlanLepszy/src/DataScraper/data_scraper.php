f010309846e5d5c6af95c93dc60e29f65de61aae
Dawid Dec <dd48671>
2024-11-28 03:02:32 +0100

    Inicjalizacja branch'a do data scraping'u

--- PlanLepszy/src/DataScraper/data_scraper.php
+++ PlanLepszy/src/DataScraper/data_scraper.php
@@ -1,2 +1,18 @@
 <?php
 
+ini_set('display_errors', true);
+
+//$url = 'https://plan.zut.edu.pl/schedule_student.php?number=48671&start=2024-11-25&end=2024-12-02';
+//$url = 'https://plan.zut.edu.pl/schedule.php?kind=teacher&query=+';
+//$url = 'https://plan.zut.edu.pl/schedule.php?kind=room&query=+';
+$url = 'https://plan.zut.edu.pl/schedule.php?kind=subject&query=+';
+//$url = 'https://plan.zut.edu.pl/schedule.php?kind=group&query=+';
+
+$response = file_get_contents($url);
+if (! $response) {
+    die("No response.");
+}
+var_dump($response);
+
+$json = json_decode($response);
+var_dump($json);
