543ef6ff857eebcea8b78dfd63697b271a5f86fb
Dawid Dec <dd48671>
2024-11-28 06:34:19 +0100

    Początek funkcjonalności data scraping'u

--- PlanLepszy/DataScraper/DATA_SCRAPER.bat
+++ PlanLepszy/DataScraper/DATA_SCRAPER.bat
@@ -1,2 +1,1 @@
-
-php data_scraper.php;
+php .\data_scraper.php;
--- PlanLepszy/DataScraper/data_scraper.php
+++ PlanLepszy/DataScraper/data_scraper.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\DataScraper;
-require '..\Entity\Teacher.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 use App\Entity\Lesson;
 use App\Entity\Teacher;
+use App\Entity\Subject;
+use App\Entity\Room;
 use Doctrine\ORM\EntityManagerInterface;
 
 //return 0;
@@ -13,41 +15,87 @@ ini_set('display_errors', true);
 
 //$url = 'https://plan.zut.edu.pl/schedule_student.php?number=48671&start=2024-11-25&end=2024-12-02';
 
-//$url = 'https://plan.zut.edu.pl/schedule.php?kind=room&query=+';
-//url = 'https://plan.zut.edu.pl/schedule.php?kind=subject&query=+';
 //$url = 'https://plan.zut.edu.pl/schedule.php?kind=group&query=+';
 
 $url = 'https://plan.zut.edu.pl/schedule.php?kind=teacher&query=+';
 $response = file_get_contents($url);
 $json = json_decode($response);
 //var_dump($json);
-
 foreach($json as $obj){
     //var_dump($obj->item);
     //print $obj->item;
-
-    //$teacher = $entityManager->getRepository(Teacher::class)->find($obj->item);
-    //if (!$teacher) {
-    //
-    //}
 
     $teacher = new Teacher();
     $teacher->setItem($obj->item);
 
     $tok = strtok($obj->item, " ");
     $teacher_string = $tok;
+    $tok = strtok(" ");
     while ($tok !== false) {
         $teacher_string .= ("%20" . $tok);
         $tok = strtok(" ");
     }
-    $url = "https://plan.zut.edu.pl/schedule_student.php?nteacher=$teacher_string&start=2024-11-25&end=2024-12-02";
+    $url = "https://plan.zut.edu.pl/schedule_student.php?teacher=$teacher_string&start=2024-11-25&end=2024-12-02";
     $response = file_get_contents($url);
     $json2 = json_decode($response);
     foreach($json2 as $obj2){
-        //$lesson = new Lesson();
-        //$lesson->setItem($obj2->item);
-        var_dump($obj2);
-        //$teacher->addLesson($lesson);
+        $item = json_encode($obj2);
+        var_dump($item);
+        $lesson = new Lesson();
+        $lesson->setItem($item);
+        $teacher->addLesson($lesson);
+    }
+}
+
+$url = 'https://plan.zut.edu.pl/schedule.php?kind=subject&query=+';
+$response = file_get_contents($url);
+$json = json_decode($response);
+foreach($json as $obj){
+    $subject = new Subject();
+    $subject->setItem($obj->item);
+
+    $tok = strtok($obj->item, " ");
+    $subject_string = $tok;
+    $tok = strtok(" ");
+    while ($tok !== false) {
+        $subject_string .= ("%20" . $tok);
+        $tok = strtok(" ");
+    }
+    $url = "https://plan.zut.edu.pl/schedule_student.php?subject=$subject_string&start=2024-11-25&end=2024-12-02";
+    $response = file_get_contents($url);
+    $json2 = json_decode($response);
+    foreach($json2 as $obj2){
+        $item = json_encode($obj2);
+        var_dump($item);
+        $lesson = new Lesson();
+        $lesson->setItem($item);
+        $subject->addLesson($lesson);
+    }
+}
+
+$url = 'https://plan.zut.edu.pl/schedule.php?kind=room&query=+';
+$response = file_get_contents($url);
+$json = json_decode($response);
+foreach($json as $obj){
+    $room = new Room();
+    $room->setItem($obj->item);
+
+    $tok = strtok($obj->item, " ");
+    $room_string = $tok;
+    $tok = strtok(" ");
+    while ($tok !== false) {
+        $room_string .= ("%20" . $tok);
+        $tok = strtok(" ");
+    }
+    $url = "https://plan.zut.edu.pl/schedule_student.php?room=$room_string&start=2024-11-25&end=2024-12-02";
+    $response = file_get_contents($url);
+    $json2 = json_decode($response);
+    foreach($json2 as $obj2){
+        $item = json_encode($obj2);
+        var_dump($item);
+        $lesson = new Lesson();
+        $lesson->setItem($item);
+        $teacher->addLesson($lesson);
     }
 }
 
